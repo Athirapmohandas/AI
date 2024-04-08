48a6a7dce395034ea7d9b61862f1eea3f2c78da9
Athirapmohandas <88778618+Athirapmohandas@users.noreply.github.com>
2024-04-08 11:56:57 +0530

    Update pdf.py

--- pdf.py
+++ pdf.py
@@ -1,64 +1,55 @@
-import streamlit as st
-from langchain.chains import RetrievalQA
-from langchain.llms import OpenAI
-from langchain.embeddings.openai import OpenAIEmbeddings
-from langchain.vectorstores import Qdrant
-import qdrant_client
-import openai
-import os
-from langchain.agents import load_tools
-from langchain.agents import initialize_agent
-from langchain.agents import AgentType
 from dotenv import load_dotenv
+import streamlit as st
+from PyPDF2 import PdfReader
+from langchain.text_splitter import CharacterTextSplitter
+from langchain.embeddings.openai import OpenAIEmbeddings
+from langchain.vectorstores import FAISS
+from langchain.chains.question_answering import load_qa_chain
+from langchain.llms import OpenAI
+from langchain.callbacks import get_openai_callback
 
-
-def get_vector_store():
-    client = qdrant_client.QdrantClient(
-    os.getenv('QDRANT_HOST'),
-    api_key = os.getenv('QDRANT_API_KEY')
-    )
-    embeddings =OpenAIEmbeddings()
-    vector_store = Qdrant(
-    client=client, 
-    collection_name=os.getenv('QDRANT_COLLECTION_NAME'), 
-    embeddings=embeddings
-    )
-    return vector_store
 
 def main():
     load_dotenv()
-
-    st.set_page_config(page_title="Ask Qdrant and OpenAI")
-    st.header("Ask your remote database")
-
-    vector_store = get_vector_store()
-
-    llm = OpenAI(temperature=0.6,model="gpt-3.5-turbo-instruct")  # Initialize OpenAI LLM with desired temperature
-
-    # tools = load_tools(["serpapi"],llm=llm)
-
-    openai.api_key = os.getenv("OPENAI_API_KEY")  # Set OpenAI API key
-
-    qa = RetrievalQA.from_chain_type(
-        llm=llm,
-        chain_type="stuff",
-        retriever=vector_store.as_retriever(),
-    )
-
-    user_question = st.text_input("Ask a question:")
-    if user_question:
+    st.set_page_config(page_title="Ask your PDF")
+    st.header("Ask your PDF 💬")
+    
+    # upload file
+    pdf = st.file_uploader("Upload your PDF", type="pdf")
+    
+    # extract the text
+    if pdf is not None:
+      pdf_reader = PdfReader(pdf)
+      text = ""
+      for page in pdf_reader.pages:
+        text += page.extract_text()
         
-            answer = qa.run(user_question)
-            st.write(f"Answer from database: {answer}")
+      # split into chunks
+      text_splitter = CharacterTextSplitter(
+        separator="\n",
+        chunk_size=1000,
+        chunk_overlap=200,
+        length_function=len
+      )
+      chunks = text_splitter.split_text(text)
+      
+      # create embeddings
+      embeddings = OpenAIEmbeddings()
+      knowledge_base = FAISS.from_texts(chunks, embeddings)
+      
+      # show user input
+      user_question = st.text_input("Ask a question about your PDF:")
+      if user_question:
+        docs = knowledge_base.similarity_search(user_question)
+        
+        llm = OpenAI()
+        chain = load_qa_chain(llm, chain_type="stuff")
+        with get_openai_callback() as cb:
+          response = chain.run(input_documents=docs, question=user_question)
+          print(cb)
+           
+        st.write(response)
+    
 
-            if "I don't know" in answer:
-                #  ##################### Answer from chatgpt
-                st.write("Attempting to answer using OpenAI...")
-                openai_answer = llm(user_question)
-                st.write(f"Answer from OpenAI: {openai_answer}")
-                 
-            # else:
-            #     st.write(f"Answer from Qdrant: {answer}")
-
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
